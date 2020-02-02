3c46baa2edd875f8440fe41ebefb6439116e1eb2
John MacDonald <john.l.macdonald.22@dartmouth.edu>
2020-02-02 22:52:00 +0000

    final changes

--- catkin_ws/src/kalman_filter/src/generate_graph.py
+++ catkin_ws/src/kalman_filter/src/generate_graph.py
@@ -8,7 +8,6 @@ class Graph:
 		self.GROUND_TRUTH_INITIAL = 0.0 # measured starting point (robot reference frame)
 		self.GROUND_TRUTH_FINAL = 1.025 # measured ending point (robot reference frame)
 
-
 		self.raw_file = open("calculated_coords.txt").read().split("\n")
 		self.pose_file = open("kalman_with_pose_input_coords.txt").read().split("\n")
 		self.cmd_vel_file = open("kalman_with_cmd_vel_input_coords.txt").read().split("\n")
@@ -18,16 +17,24 @@ class Graph:
 		self.cv_coords = [] # kalman filter with cmd_vel and scan input
 		# print(self.pose_file)
 
+		self.calc_start = float("-inf")
+		self.pose_start = float("-inf")
+		self.cv_start = float("-inf")
+
 		for row in self.pose_file:
 			if row != '':
 				data = row.split("\t")
 				if float(data[0]) > 0:
+					if self.pose_start == float("-inf"):
+						self.calc_start = data[0]
 					self.pose_coords.append((data[0], data[1]))
 
 		for row_2 in self.cmd_vel_file:
 			if row_2 != '':
 				data = row_2.split("\t")
 				if float(data[0]) > 0:
+					if self.cv_start == float("-inf"):
+						self.cv_start = data[0]
 					self.cv_coords.append((data[0], data[1]))
 
 
@@ -35,6 +42,8 @@ class Graph:
 			if row_3 != '':
 				data = row_3.split("\t")
 				if float(data[0]) > 0:
+					if self.calc_start == float("-inf"):
+						self.calc_start = data[0]
 					self.calc_coords.append((data[0], data[1]))
 
 		# final coordinate from each of the filters and raw data
@@ -46,9 +55,16 @@ class Graph:
 	def drawError(self):
 		# draws the graph of the start and end position of the robot relative to the ground truth
 		print("Constructing error graph...")
+		plt.plot([[self.calc_start, 0]], 'bo')
+		plt.plot([[self.pose_start, 0]], 'go')
+		plt.plot([[self.cv_start, 0]], 'ro')
+
 		plt.plot(self.calc_end, 'bo')
 		plt.plot(self.pose_end, 'go')
 		plt.plot(self.cv_end, 'ro')
+
+		plt.plot(self.GROUND_TRUTH_INITIAL, 'yo')
+		plt.plot(self.GROUND_TRUTH_FINAL, 'yo')
 		# print(self.cv_end)
 		plt.show()
 	def draw(self):
